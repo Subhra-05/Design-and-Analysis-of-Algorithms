65680da17c671a678276fcd6e2a72ec3d4a9abcc
Subhrajeet Ganguly <subhrajeetgang2005@gmail.com>
2025-08-08 14:39:39 +0530

    EXP 3.c

---
+++ EXP 3.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+void printSubarray(int arr[], int low, int high) {
+
+	printf("Pass: ");
+	for (int i = low; i <= high; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+void printFullArray(int arr[], int size) {
+
+	for (int i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+
+void swap(int* a, int* b) {
+
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+
+	int pivot = arr[high];
+
+	int i = low - 1;
+	for (int j = low; j < high; j++) {
+		if (arr[j] <= pivot) {
+			i++;
+			swap(&arr[i], &arr[j]);
+		}
+	}
+
+
+	swap(&arr[i + 1], &arr[high]);
+	return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+
+	if (low < high) {
+		int pi = partition(arr, low, high);
+		printSubarray(arr, low, high);  // Print only subarray being processed
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+int main() {
+	int n;
+	printf("number of elements: ");
+	scanf("%d", &n);
+
+	int arr[n];
+	printf("elements: ");
+
+	for (int i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+
+	printf("Original array: ");
+	printFullArray(arr, n);
+
+	quickSort(arr, 0, n - 1);
+	printf("Sorted array: ");
+	printFullArray(arr, n);
+	return 0;
+}
+
